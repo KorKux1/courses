5b73b793f68d42cb5b4d63a63336376d0c20a104
Cristhian Castillo <cristhianeduardo03@hotmail.com>
2020-04-30 16:13:29 -0500

    Added Token auth

--- API REST/Examples/server.php
+++ API REST/Examples/server.php
@@ -2,118 +2,135 @@
 
 header( 'Content-Type: application/json' );
 
-if ( 
-    # La letra X se refiere a encabezados no estandar
-	!array_key_exists('HTTP_X_HASH', $_SERVER) || 
-	!array_key_exists('HTTP_X_TIMESTAMP', $_SERVER) || 
-	!array_key_exists('HTTP_X_UID', $_SERVER)  
-    )  {
-		header( 'Status-Code: 403' );
+if ( !array_key_exists( 'HTTP_X_TOKEN', $_SERVER ) ) {
+
+	die;
+}
+
+$url = 'https://'.$_SERVER['HTTP_HOST'].'/auth';
+
+$ch = curl_init( $url );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, [
+	"X-Token: {$_SERVER['HTTP_X_TOKEN']}",
+]);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+$ret = curl_exec( $ch );
+
+if ( curl_errno($ch) != 0 ) {
+	die ( curl_error($ch) );
+}
+
+if ( $ret !== 'true' ) {
+	http_response_code( 403 );
+
+	die;
+}
+
+$allowedResourceTypes = [
+	'books',
+	'authors',
+	'genres',
+];
+
+$resourceType = $_GET['resource_type'];
+if ( !in_array( $resourceType, $allowedResourceTypes ) ) {
+	http_response_code( 400 ); // Da un error 400
+	echo json_encode(
+		[
+			'error' => "$resourceType is un unkown",
+		]
+	);
 	
+	die;
+}
+
+$books = [
+	1 => [
+		'titulo' => 'Lo que el viento se llevo',
+		'id_autor' => 2,
+		'id_genero' => 2,
+	],
+	2 => [
+		'titulo' => 'La Iliada',
+		'id_autor' => 1,
+		'id_genero' => 1,
+	],
+	3 => [
+		'titulo' => 'La Odisea',
+		'id_autor' => 1,
+		'id_genero' => 1,
+	],
+];
+
+$resourceId = array_key_exists('resource_id', $_GET ) ? $_GET['resource_id'] : '';
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ( strtoupper( $method ) ) {
+	case 'GET':
+		if ( "books" !== $resourceType ) {
+			http_response_code( 404 );
+
+			echo json_encode(
+				[
+					'error' => $resourceType.' not yet implemented :(',
+				]
+			);
+
+			die;
+		}
+
+		if ( !empty( $resourceId ) ) {
+			if ( array_key_exists( $resourceId, $books ) ) {
+				echo json_encode(
+					$books[ $resourceId ]
+				);
+			} else {
+				http_response_code( 404 );
+
+				echo json_encode(
+					[
+						'error' => 'Book '.$resourceId.' not found :(',
+					]
+				);
+			}
+		} else {
+			echo json_encode(
+				$books
+			);
+		}
+
+		die;
+		
+		break;
+	case 'POST':
+		$json = file_get_contents( 'php://input' );
+
+		$books[] = json_decode( $json );
+
+		echo array_keys($books)[count($books)-1];
+		break;
+	case 'PUT':
+		if ( !empty($resourceId) && array_key_exists( $resourceId, $books ) ) {
+			$json = file_get_contents( 'php://input' );
+			
+			$books[ $resourceId ] = json_decode( $json, true );
+
+			echo $resourceId;
+		}
+		break;
+	case 'DELETE':
+		if ( !empty($resourceId) && array_key_exists( $resourceId, $books ) ) {
+			unset( $books[ $resourceId ] );
+		}
+		break;
+	default:
+	http_response_code( 404 );
+
 		echo json_encode(
 			[
-				'error' => "No autorizado",
+				'error' => $method.' not yet implemented :(',
 			]
 		);
-		
-		die;
-    }
-    
-list( $hash, $uid, $timestamp) = [
-    $_SERVER['HTTP_X_HASH'],
-    $_SERVER['HTTP_X_TIMESTAMP'],
-    $_SERVER['HTTP_X_UID'],
-];
 
-$secret = 'Sh!! No se lo cuentes a nadie!';
-
-$newHash = sha1($uid.$timestamp.$secret);
-
-if($newHash !== $hash) {
-    die;
-}
-
-// Se define los recursos disponibles
-$allowedResorceTypes = [
-    'books',
-    'authors',
-    'genres'
-];
-
-
-if ( $user !== 'korkux' || $password !== '123') {
-    echo "Logueate para poder usar la aplicación";
-}
-
-# Verificar si el tipo de recurso solicitado está dentro de los posibles
-$resourceType = $_GET['resource_type'];
-
-if(!in_array($resourceType, $allowedResorceTypes)){
-    die;
-}
-
-//Defino los recursos
-$books = [
-    1 => [
-        'title' =>  'Lo que el viento se llevo',
-        'id_author' => 2,
-        'id_genre' => 2
-    ],
-    2 => [
-        'title' =>  'La Iliada',
-        'id_author' => 1,
-        'id_genre' => 1
-    ],
-    3 => [
-        'title' =>  'La Odisea',
-        'id_author' => 1,
-        'id_genre' => 1
-    ],
-];
-
-header('Content-Type: application/json');
-
-// Obtenemos el Id del recurso
-$resourceId = array_key_exists('resource_id', $_GET) ? $_GET['resource_id']: '';
-
-// Generamos la respuesta asumiendo que el pedido es correcto
-switch( strtoupper($_SERVER['REQUEST_METHOD']) ) {
-    case 'GET':
-        if ( empty( $resourceId ) ){
-            echo json_encode( $books );
-        }else{
-             // si llegan a pedir un recurso en especifico
-            if( array_key_exists( $resourceId, $books) ){
-                echo json_encode( $books[ $resourceId ] );
-            }
-        }
-        break;
-    case 'POST':
-        $json = file_get_contents('php://input');
-        $books[] = json_decode($json, true);
-
-        echo array_keys($books)[count($books)-1];
-    break;
-    case 'PUT':
-        // Validamos que el recurso enviado exista
-        if (!empty($resourceId) && array_key_exists($resourceId, $books)){
-            // Tomamos la entrada cruda
-            $json = file_get_contents('php://input');
-             // transformamos el json recibido a un nuevo elemento del arreglo
-            $books[$resourceId] = json_decode($json, true);
-            // Retornamos la coleccion modificada en formato json
-            echo json_encode($books);
-        }
-    break;
-    case 'DELETE':
-        if (!empty($resourceId) && array_key_exists($resourceId, $books)){
-            // Eliminamos el recurso
-            unset( $books[ $resourceId]);    
-            // Aquí verificamos que los cambios se han realizado  
-            echo json_encode($books);      
-        }
-    break;
-    default:
-        # code...
-        break;
+		break;
 }
