42c8341a1e65e5c3153a8cf9dbc9f8bd3f43c60d
KorKux <cristhianeduardo03@hotmail.com>
2020-05-05 13:04:20 -0500

    Added example of macros

--- Flask/platzi-flask/main.py
+++ Flask/platzi-flask/main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)  # __name__ indica que el nombre de la aplicación será el nombre del archivo
 
-todos = ['TODO 1', 'TODO 2', 'TODO 3']
+todos = ['Comprar Café', 'Enviar solicitud de compra', 'Entregar video al productor']
 @app.route('/')
 def index():
     user_ip = request.remote_addr  # Remote address es la IP del usuario.
