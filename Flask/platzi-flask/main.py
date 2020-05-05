b11993ae51de1515b37942dd6588e8438e0a7583
Cristhian Castillo <cristhianeduardo03@hotmail.com>
2020-05-05 08:36:50 -0500

    Added example of conditionals and iterations, using context

--- Flask/platzi-flask/main.py
+++ Flask/platzi-flask/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)  # __name__ indica que el nombre de la aplicación será el nombre del archivo
 
+todos = ['TODO 1', 'TODO 2', 'TODO 3']
 @app.route('/')
 def index():
     user_ip = request.remote_addr  # Remote address es la IP del usuario.
@@ -13,4 +14,8 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return render_template('hello.html', user_ip=user_ip)
+    context = {
+        'user_ip':user_ip,
+        'todos':todos
+    }
+    return render_template('hello.html', **context) #Los ** expanden el diccionario.
