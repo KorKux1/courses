2e8850c2dcf87e26563739bf6813a985d2ced99c
Cristhian Castillo <cristhianeduardo03@hotmail.com>
2020-05-05 08:15:59 -0500

    Added example of Temples with Jinja 2

--- Flask/platzi-flask/main.py
+++ Flask/platzi-flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)  # __name__ indica que el nombre de la aplicación será el nombre del archivo
 
@@ -13,4 +13,4 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return "Hello, World Flask, tú IP es: {}".format(user_ip)
+    return render_template('hello.html', user_ip=user_ip)
