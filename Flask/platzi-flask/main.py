3000476f832b4897d00e7d07459132299ffd2fdf
Cristhian Castillo <cristhianeduardo03@hotmail.com>
2020-05-06 08:25:24 -0500

    Added LoginForm

--- Flask/platzi-flask/main.py
+++ Flask/platzi-flask/main.py
@@ -1,10 +1,20 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import  StringField, PasswordField, SubmitField
+from wtforms.validators import  DataRequired
 
 app = Flask(__name__)  # __name__ indica que el nombre de la aplicación será el nombre del archivo
 bootstrap = Bootstrap(app)
 
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
 todos = ['Comprar Café', 'Enviar solicitud de compra', 'Entregar video al productor']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    submit = SubmitField('Entrar')
 
 """"
 Errores
@@ -20,18 +30,22 @@ def server_error(error):
 """"
 Rutas
 """
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr  # Remote address es la IP del usuario.
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip':user_ip,
-        'todos':todos
+        'todos':todos,
+        'login_form': login_form
     }
     return render_template('hello.html', **context) #Los ** expanden el diccionario.
