76dfd723f80e57771bdf7021948e07a17139c1c1
Cristhian Castillo <cristhianeduardo03@hotmail.com>
2020-05-06 09:22:40 -0500

    Added Flashes example

--- Flask/platzi-flask/main.py
+++ Flask/platzi-flask/main.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import  StringField, PasswordField, SubmitField
 from wtforms.validators import  DataRequired
+import unittest
 
 app = Flask(__name__)  # __name__ indica que el nombre de la aplicación será el nombre del archivo
 bootstrap = Bootstrap(app)
@@ -30,8 +31,6 @@ def server_error(error):
 """"
 Rutas
 """
-
-
 @app.route('/')
 def index():
     user_ip = request.remote_addr  # Remote address es la IP del usuario.
@@ -39,13 +38,31 @@ def index():
     session['user_ip'] = user_ip
     return response
 
+
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
     login_form = LoginForm()
+    username = session.get('username')
+    
+
     context = {
         'user_ip':user_ip,
         'todos':todos,
-        'login_form': login_form
+        'login_form': login_form,
+        'username': username
     }
+
+    if login_form.validate_on_submit(): 
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usuario registrado con éxito')
+
+        return redirect(url_for('index'))
+
     return render_template('hello.html', **context) #Los ** expanden el diccionario.
+
+
+
+
